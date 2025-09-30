1ad9ec50d92ca0d9b4e7bb5f3d7ef2a1f14ddda5
MrFast <mrfastkrunker@gmail.com>
2025-09-29 21:30:27 -0400

    Add drawAsRoot method for VexelElement

--- src/main/kotlin/xyz/meowing/vexel/components/base/VexelElement.kt
+++ src/main/kotlin/xyz/meowing/vexel/components/base/VexelElement.kt
@@ -2,12 +2,15 @@ package xyz.meowing.vexel.components.base
 
 import net.minecraft.client.util.Window
 import xyz.meowing.vexel.Vexel.mc
+import xyz.meowing.vexel.animations.AnimationManager
 import xyz.meowing.vexel.core.VexelWindow
 import xyz.meowing.vexel.animations.EasingType
 import xyz.meowing.vexel.animations.fadeIn
 import xyz.meowing.vexel.animations.fadeOut
 import xyz.meowing.vexel.components.core.Rectangle
 import xyz.meowing.vexel.components.core.Tooltip
+import xyz.meowing.vexel.utils.MouseUtils
+import xyz.meowing.vexel.utils.render.NVGRenderer
 
 enum class Size {
     Auto,
@@ -85,6 +88,16 @@ abstract class VexelElement<T : VexelElement<T>>(
         mouseClickListeners.clear()
         mouseReleaseListeners.clear()
         charTypeListeners.clear()
+    }
+
+    fun drawAsRoot() {
+        NVGRenderer.push()
+        NVGRenderer.beginFrame(mc.window.scaledWidth.toFloat(), mc.window.scaledHeight.toFloat())
+        NVGRenderer.push()
+        render(MouseUtils.scaledX.toFloat(), MouseUtils.scaledY.toFloat())
+        AnimationManager.update()
+        NVGRenderer.pop()
+        NVGRenderer.endFrame()
     }
 
     fun findFirstVisibleParent(): VexelElement<*>? {
