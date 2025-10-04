b2b795e7346df2b300b1505933e9f94d031173a6
aurielyn <aurielyn.dev@gmail.com>
2025-10-04 11:51:25 +0530

    Bump version | Split VexelElement into multiple files | Add renderDebugHitbox method

---
+++ src/main/kotlin/xyz/meowing/vexel/components/base/Enums.kt
@@ -0,0 +1,18 @@
+package xyz.meowing.vexel.components.base
+
+enum class Size {
+    Auto,
+    ParentPerc,
+    Pixels
+}
+
+enum class Pos {
+    ParentPercent,
+    ScreenPercent,
+    ParentPixels,
+    ScreenPixels,
+    ParentCenter,
+    ScreenCenter,
+    AfterSibling,
+    MatchSibling
+}
