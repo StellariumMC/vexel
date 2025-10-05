0bbafe6ea5b6ff2104d4b3e77ec8eb8a881a36b4
aurielyn <aurielyn.dev@gmail.com>
2025-10-05 15:33:21 +0530

    Go back to DGT because gah damn arch loom is too much for my dumbahh

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,100 +1,55 @@
 plugins {
-    idea
     java
-    signing
-    `maven-publish`
-    id("gg.essential.loom") version "0.10.0.+"
-    id("dev.architectury.architectury-pack200") version "0.1.3"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm")
+    id("dev.deftu.gradle.multiversion")
+    id("dev.deftu.gradle.tools")
+    id("dev.deftu.gradle.tools.resources")
+    id("dev.deftu.gradle.tools.bloom")
+    id("dev.deftu.gradle.tools.shadow")
+    id("dev.deftu.gradle.tools.minecraft.loom")
+    id("dev.deftu.gradle.tools.minecraft.releases")
+    id("dev.deftu.gradle.tools.publishing.maven")
 }
 
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
-    withSourcesJar()
-    withJavadocJar()
-}
-
-loom {
-    forge {
-        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
-    }
-}
-
-tasks.compileJava {
-    dependsOn(tasks.processResources)
-}
-
-sourceSets.main {
-    output.setResourcesDir(sourceSets.main.flatMap { it.java.classesDirectory })
-    java.srcDir(layout.projectDirectory.dir("src/main/kotlin"))
-    kotlin.destinationDirectory.set(java.destinationDirectory)
-}
-
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://maven.deftu.dev/snapshots")
-    maven("https://maven.deftu.dev/releases")
-}
-
-val shadowImpl: Configuration by configurations.creating {
-    configurations.implementation.get().extendsFrom(this)
+toolkitMultiversion {
+    moveBuildsToRootProject.set(true)
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.8.9")
-    mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
-    forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
-
     implementation(kotlin("stdlib-jdk8"))
-
-    compileOnly("com.github.odtheking:odin-lwjgl:68de0d3e0b")
 
     api("org.lwjgl:lwjgl-nanovg:3.3.3")
     api("org.lwjgl:lwjgl-stb:3.3.3")
-    shadowImpl("dev.deftu:isolated-lwjgl3-loader:0.3.2") {
+    api(shade("dev.deftu:isolated-lwjgl3-loader:0.3.2") {
         exclude(group = "org.apache")
         exclude(group = "org.intellij")
         exclude(group = "org.jetbrains")
+    })
+    modApi(shade("xyz.meowing:knit-${mcData}:101")!!)
+}
+
+toolkitMavenPublishing {
+    artifactName.set("vexel")
+    setupRepositories.set(false)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
 
-tasks.withType(JavaCompile::class) {
-    options.encoding = "UTF-8"
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(8)
 }
-
-tasks.shadowJar {
-    archiveClassifier.set("dev")
-    configurations = listOf(shadowImpl)
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-val remapJar by tasks.named<net.fabricmc.loom.task.RemapJarTask>("remapJar") {
-    archiveClassifier.set("")
-    dependsOn(tasks.shadowJar)
-    from(zipTree(tasks.shadowJar.get().archiveFile))
-}
-
-tasks.jar {
-    archiveClassifier.set("thin")
-    destinationDirectory.set(layout.buildDirectory.dir("devlibs"))
-}
-
-tasks.assemble.get().dependsOn(tasks.remapJar)
 
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("mavenJava") {
-                artifactId = "vexel-1.8.9-forge"
-                groupId = project.properties["mod.group"] as String
-                version = project.properties["mod.version"] as String
-
-                artifact(tasks.remapJar.get().archiveFile)
-                artifact(tasks["sourcesJar"])
-                artifact(tasks["javadocJar"])
-
+            named<MavenPublication>("mavenJava") {
                 pom {
                     name.set("Vexel")
                     description.set("A simple declarative rendering library built with lwjgl's NanoVG Renderer")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,37 @@
 pluginManagement {
     repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
-        maven("https://maven.architectury.dev/")
+        maven("https://maven.deftu.dev/snapshots")
+        maven("https://maven.deftu.dev/releases")
         maven("https://maven.fabricmc.net")
-        maven("https://maven.minecraftforge.net/")
-        maven("https://repo.spongepowered.org/maven/")
-        maven("https://repo.essential.gg/repository/maven-releases/")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.minecraftforge.net")
+        maven("https://repo.essential.gg/repository/maven-public")
+        maven("https://server.bbkr.space/artifactory/libs-release/")
+        maven("https://jitpack.io/")
+
+        mavenLocal()
+        mavenCentral()
+
+        gradlePluginPortal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "gg.essential.loom" -> useModule("gg.essential:architectury-loom:${requested.version}")
-            }
-        }
+
+    plugins {
+        kotlin("jvm") version("2.0.0")
+        id("dev.deftu.gradle.multiversion-root") version("2.57.0")
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
-}
+val projectName: String = extra["mod.name"].toString()
 
-rootProject.name = extra["mod.name"].toString()
+rootProject.name = projectName
+rootProject.buildFileName = "root.gradle.kts"
+
+listOf(
+    "1.8.9-forge"
+).forEach { version ->
+    include(":$version")
+    project(":$version").apply {
+        projectDir = file("versions/$version")
+        buildFileName = "../../build.gradle.kts"
+    }
+}
