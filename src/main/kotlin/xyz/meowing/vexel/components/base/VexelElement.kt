987452078944a80f160362fc1733103334cb8371
Eclipse-5214 <elijahlipscomb55@gmail.com>
2025-10-08 12:32:15 -0400

    make it better

--- src/main/kotlin/xyz/meowing/vexel/components/base/VexelElement.kt
+++ src/main/kotlin/xyz/meowing/vexel/components/base/VexelElement.kt
@@ -48,11 +48,32 @@ abstract class VexelElement<T : VexelElement<T>>(
             invalidateChildrenSizes()
         }
 
-    val scaleFactor get() = KnitResolution.scaleFactor.toFloat()
-    val left: Float get() = x / scaleFactor
-    val top: Float get() = y / scaleFactor
-    val right: Float get() = (x + width) / scaleFactor
-    val bottom: Float get() = (y + height) / scaleFactor
+    inner class Scaled {
+        val scaleFactor get() = KnitResolution.scaleFactor.toFloat()
+
+        val left: Float get() = x / scaleFactor
+        val top: Float get() = y / scaleFactor
+        val right: Float get() = (x + width) / scaleFactor
+        val bottom: Float get() = (y + height) / scaleFactor
+        val centerX: Float get() = (left + right) / 2f
+        val centerY: Float get() = (top + bottom) / 2f
+        val width: Float get() = this@VexelElement.width / scaleFactor
+        val height: Float get() = this@VexelElement.height / scaleFactor
+    }
+
+    inner class Raw {
+        val left get() = x
+        val top get() = y
+        val right get() = x + width
+        val bottom get() = y + height
+        val centerX get() = (left + right) / 2
+        val centerY get() = (top + bottom) / 2
+        val width get() = this@VexelElement.width
+        val height get() = this@VexelElement.height
+    }
+
+    val raw = Raw()
+    val scaled = Scaled()
 
     var widthPercent: Float = 100f
     var heightPercent: Float = 100f
