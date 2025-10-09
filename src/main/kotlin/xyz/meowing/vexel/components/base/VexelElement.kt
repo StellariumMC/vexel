1b1ced6b03501f461ce07b32b2982f60f4d02153
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2025-10-09 10:00:15 -0400

    Add a lot of helper methods

--- src/main/kotlin/xyz/meowing/vexel/components/base/VexelElement.kt
+++ src/main/kotlin/xyz/meowing/vexel/components/base/VexelElement.kt
@@ -1,6 +1,7 @@
 package xyz.meowing.vexel.components.base
 
 import xyz.meowing.knit.api.input.KnitMouse
+import xyz.meowing.knit.api.render.KnitResolution
 import xyz.meowing.vexel.Vexel.renderEngine
 import xyz.meowing.vexel.animations.AnimationManager
 import xyz.meowing.vexel.animations.EasingType
@@ -46,6 +47,33 @@ abstract class VexelElement<T : VexelElement<T>>(
             invalidateChildrenPositions()
             invalidateChildrenSizes()
         }
+
+    inner class Scaled {
+        val scaleFactor get() = KnitResolution.scaleFactor.toFloat()
+
+        val left: Float get() = x / scaleFactor
+        val top: Float get() = y / scaleFactor
+        val right: Float get() = (x + width) / scaleFactor
+        val bottom: Float get() = (y + height) / scaleFactor
+        val centerX: Float get() = (x + width / 2f) / scaleFactor
+        val centerY: Float get() = (y + height / 2f) / scaleFactor
+        val width: Float get() = this@VexelElement.width / scaleFactor
+        val height: Float get() = this@VexelElement.height / scaleFactor
+    }
+
+    inner class Raw {
+        val left get() = x
+        val top get() = y
+        val right get() = x + width
+        val bottom get() = y + height
+        val centerX get() = (left + right) / 2f
+        val centerY get() = (top + bottom) / 2f
+        val width get() = this@VexelElement.width
+        val height get() = this@VexelElement.height
+    }
+
+    val raw = Raw()
+    val scaled = Scaled()
 
     var widthPercent: Float = 100f
     var heightPercent: Float = 100f
---
+++ src/main/kotlin/xyz/meowing/vexel/utils/render/NVGRenderer.kt
@@ -0,0 +1,6 @@
+package xyz.meowing.vexel.utils.render
+
+import xyz.meowing.vexel.Vexel
+
+// low-key jank but good for continuity ¯\_(ツ)_/¯
+val NVGRenderer = Vexel.renderEngine
