38357b9232546286de217e85a8c1aec82fbdb7dc
aurielyn <aurielyn.dev@gmail.com>
2025-10-13 13:56:51 +0530

    Bump Knit to 104

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
         exclude(group = "org.intellij")
         exclude(group = "org.jetbrains")
     })
-    modApi(shade("xyz.meowing:knit-${mcData}:103")!!)
+    modApi(shade("xyz.meowing:knit-${mcData}:104")!!)
 }
 
 toolkitMavenPublishing {
